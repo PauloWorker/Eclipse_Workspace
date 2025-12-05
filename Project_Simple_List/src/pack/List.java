1abbeec9da3edc177c1eee9c8403922dd9ae8999
PauloWorker <pauloribierotrabalho@gmail.com>
2025-12-05 13:56:13 -0300

    Added files

--- Project_Simple_List/src/pack/List.java
+++ Project_Simple_List/src/pack/List.java
@@ -1,6 +1,23 @@
 package pack;
 
+import java.time.LocalDateTime;
+
 public class List {
 	String task;
+	private final LocalDateTime currentTime = LocalDateTime.now();
+	private String s = currentTime.toString(); //Convert the LocalDateTime to String
+	private String date = s.substring(0, s.indexOf('T')); //Get the Date
+	private String time = s.substring(s.indexOf('T') + 1, s.indexOf('.')); //Get the Time
 	
+	List(String task)
+	{
+		this.task = task;
+	}
+	
+	public void print() //Debug
+	{
+		System.out.println("LocalDateTime: " + currentTime);
+		System.out.println("Date: " + date);
+		System.out.println("Time: " + time);
+	}
 }
--- Project_Simple_List/src/pack/Main.java
+++ Project_Simple_List/src/pack/Main.java
@@ -2,6 +2,8 @@ package pack;
 
 public class Main {
 	public static void main(String[] args) {
+		List list = new List("task");
 		
+		list.print();
 	}
 }
